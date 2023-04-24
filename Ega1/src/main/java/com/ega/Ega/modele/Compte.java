6f869f8ef6f43f4a01e828b96bb50ab6e4b95456
0528-nOUM <germainnoumonvi2002@gmail.com>
2023-04-24 11:29:23 +0000

    compte

---
+++ Ega1/src/main/java/com/ega/Ega/modele/Compte.java
@@ -0,0 +1,45 @@
+package com.ega.Ega.modele;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.context.annotation.Primary;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.UUID;
+
+enum TypeCompte{
+    EPARGNE,
+    COURANT
+}
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "Compte")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String numeroCompte;
+
+    @Enumerated(EnumType.STRING)
+    private TypeCompte typeCompte;
+
+    private LocalDate dateCreation=LocalDate.now();
+
+    private Double solde=0.0;
+
+    private  Boolean is_deleted=false;
+    @ManyToOne( fetch = FetchType.LAZY)
+    @JsonIgnoreProperties("comptes")
+    private Client client;
+
+}
