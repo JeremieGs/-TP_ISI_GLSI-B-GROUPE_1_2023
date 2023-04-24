9940a6a57729d3238488e1da30cb0dd5ca8d3a3d
0528-nOUM <germainnoumonvi2002@gmail.com>
2023-04-24 09:44:36 +0000

    client

--- Ega1/src/main/java/com/ega/Ega/modele/Client.java
+++ Ega1/src/main/java/com/ega/Ega/modele/Client.java
@@ -3,12 +3,11 @@ package com.ega.Ega.modele;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.validation.constraints.NotBlank;
+import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-
 import java.time.LocalDate;
 import java.util.List;
 
@@ -27,37 +26,37 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull(message = "le champ nom est obligatoire")
-    @NotBlank(message = "entrez  un nom valide")
+    @NotNull(message = "Ce champ est obligatoire")
+    @NotBlank(message = "Entrez  un nom valide")
     @Column(nullable = false)
     private String nom;
-    @NotEmpty(message = "le champ prenom est obligatoire")
-    @NotBlank(message = "entrez  un prenom valide")
+    @NotEmpty(message = "Ce champ est obligatoire")
+    @NotBlank(message = "Entrez  un prenom valide")
     @Column(nullable = false)
     private String prenom;
-    @NotEmpty(message = "le champ date de naissance est obligatoire")
-    @NotBlank(message = "entrez  une date de naissance valide ")
+    @NotEmpty(message = "Ce champ est obligatoire")
+    @NotBlank(message = "Entrez  une date valide ")
     @Column(nullable = false)
     private LocalDate dateNaissance;
-    @NotEmpty(message = "le champ sexe est obligatoire")
-    @NotBlank(message = "entrez  un sexe valide")
+    @NotEmpty(message = "Ce champ est obligatoire")
+    @NotBlank(message = "Entrez  un sexe valide")
     @Column(length = 2,nullable = false)
     @Enumerated(EnumType.STRING)
     private Sexe sexe;
     @Column(nullable = false)
-    @NotEmpty(message = "le champ adresse est obligatoire")
-    @NotBlank(message = "entrez  une adresse valide")
+    @NotEmpty(message = "Ce champ est obligatoire")
+    @NotBlank(message = "Entrez  une adresse valide")
     private String adresse;
-    @NotEmpty(message = "le champ telephone est obligatoire")
-    @NotBlank(message = "entrez  un numero de telephone valide")
+    @NotEmpty(message = "Ce champ est obligatoire")
+    @NotBlank(message = "Entrez  un numero de telephone valide")
     @Column(nullable = false)
     private String telephone;
-    @NotEmpty(message = "le champ courriel est obligatoire")
-    @NotBlank(message = "entrez  un courriel valide")
+    @NotEmpty(message = "Ce champ est obligatoire")
+    @NotBlank(message = "Entrez  un courriel valide")
     @Column(nullable = false,unique = true)
     private String courriel;
-    @NotEmpty(message = "le champ nationalite est obligatoire")
-    @NotBlank(message = "entrez  une nationalite valide")
+    @NotEmpty(message = "Ce champ est obligatoire")
+    @NotBlank(message = "Entrez  une nationalite valide")
     @Column(nullable = false)
     private String nationalite;
 
